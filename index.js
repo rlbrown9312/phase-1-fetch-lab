5e864e96b71c4d087bb80765595130d8ad6ec665
Richard <richard.brown0807@gmail.com>
2022-09-25 22:10:59 -0500

    commit tree fcf0962bb1922274a91eff55257eb72d836c983a

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   fetchBooks();
-  
 });
 
-function fetchBooks() {
+const fetchBooks = () => {
   return fetch("https://anapioficeandfire.com/api/books")
         .then(res => res.json())
         .then(bookData => renderBooks(bookData))
 };
 
-function renderBooks(bookData) {
-  const main = document.querySelector("main");
+const renderBooks = (bookData) => {
+  // const main = document.querySelector("main");
   
   bookData.forEach(book => {
+    const main = document.querySelector("main");
     const h3= document.createElement("h3");
     h3.textContent = book.name;
     main.appendChild(h3);
