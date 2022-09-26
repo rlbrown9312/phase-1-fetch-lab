202c3aec705fa27d2eb6eda202180016b83a5ea5
Richard <richard.brown0807@gmail.com>
2022-09-25 22:02:56 -0500

    Done With Assignment

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  return fetch('https://anapioficeandfire.com/api/books')
-  .then(res => res.json()) 
-  .then(toast => renderBooks(toast))
-
-
-}
-
-
-function renderBooks(toast) {
-  const main = document.querySelector('main');
-  toast.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", () => {
   fetchBooks();
+  
 });
+
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+        .then(res => res.json())
+        .then(bookData => renderBooks(bookData))
+};
+
+function renderBooks(bookData) {
+  const main = document.querySelector("main");
+  
+  bookData.forEach(book => {
+    const h3= document.createElement("h3");
+    h3.textContent = book.name;
+    main.appendChild(h3);
+  })
+  
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
